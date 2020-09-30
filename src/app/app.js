cdb44591cedd30d645970c6bcbb94e37a4604628
Vincent <vincentwl2309@msn.com>
2020-09-30 14:24:15 -0400

    fun when node has to carpet bomb a system to install everything.

--- src/app/app.js
+++ src/app/app.js
@@ -24,20 +24,22 @@ class BookWithReviews {
  */
 export function parseBooksData(books, reviews) {
 
-//var BooksWithReviews = books.concat(reviews);
-var BooksWithReviews;
-for(let i of books)
-{
-  if (i.id = reviews[i].bookid)
-  {
-    BooksWithReviews += books[i].concat(reviews[i]);
-  }
-}
+    //var BooksWithReviews = books.concat(reviews);
+    //var BooksWithReviews
+    //for(let i of books)
+    //{
+          
+      
+    //}
+   // var intersections = books.filter(e => reviews.indexOf(e) !== -1);
+
+    books.forEach(function (book) { book.reviews = reviews.filter(function (review) { return book.Id == review.BookId; }) }
+   // var intersection = books.filter(element => reviews.includes(element));
 
 
-  return [BooksWithReviews];  // TODO: Implement
+  //return [];  // TODO: Implement
   
-}
+
 
 /**
  * Displays data from passed `books` array. For example, if books argument would have following value:
